e0845b50949a8058791b943b7aefae3c150afa84
Nick Copeland <nickbcopeland@gmail.com>
2023-07-04 12:21:17 -0700

    set s3 bucket object_ownership to BucketOwnerPreferred to work with AWS update

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,19 +15,33 @@ provider "aws" {
     region = var.aws_region
 }
 
+
+
 # Create S3 bucket
 resource "aws_s3_bucket" "data-pipeline-bucket" {
   bucket = var.s3_bucket
   force_destroy = true
 }
 
+
+# S3 bucket object_ownership
+resource "aws_s3_bucket_ownership_controls" "data-pipeline-bucket-control" {
+  bucket = aws_s3_bucket.data-pipeline-bucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+
 # Set access control of bucket to private
 resource "aws_s3_bucket_acl" "s3_data-pipeline-bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.data-pipeline-bucket-control]
   bucket = aws_s3_bucket.data-pipeline-bucket.id
   acl    = "private"
 }
 
-# Create S3 read & write access role. This is assigned to Redshift cluster so that it can read data from S3. Write access needed for unloading data back to S3
+# Create S3 read & write access role. This is assigned to Redshift cluster so that it can read data from S3. Write access needed for unloading data back to S3t
 resource "aws_iam_role" "redshift_role" {
   name = "prod_redshift_role"
   managed_policy_arns = ["arn:aws:iam::aws:policy/AmazonS3FullAccess"]
